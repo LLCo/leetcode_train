33942d5f399aebd92a4270e8a52b4ebc14ce784f
LLCo <543476459@qq.com>
2018-12-21 12:33:20 +0800

    add 885

---
+++ 885_Spiral_Matrix_III.cpp
@@ -0,0 +1,23 @@
+/*
+
+On a 2 dimensional grid with R rows and C columns, we start at (r0, c0) facing east.
+
+Here, the north-west corner of the grid is at the first row and column, and the south-east corner of the grid is at the last row and column.
+
+Now, we walk in a clockwise spiral shape to visit every position in this grid. 
+
+Whenever we would move outside the boundary of the grid, we continue our walk outside the grid (but may return to the grid boundary later.) 
+
+Eventually, we reach all R * C spaces of the grid.
+
+Return a list of coordinates representing the positions of the grid in the order they were visited.
+
+ 
+
+Example 1:
+
+Input: R = 1, C = 4, r0 = 0, c0 = 0
+Output: [[0,0],[0,1],[0,2],[0,3]]
+
+*/
+
