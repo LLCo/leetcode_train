56ef3888772ad473d7da25684ccd1713a65f4ef7
卢怡悯 <luyimin@luyimindeMacBook-Pro.local>
2018-12-29 01:47:35 +0800

    finished 685

--- 685_Redundant_Connection_II.cpp
+++ 685_Redundant_Connection_II.cpp
@@ -30,96 +30,44 @@ Explanation: The given directed graph will be like this:
 
 using namespace std;
 
-class UnionFindSet {
-
-public:
-  UnionFindSet(int n):set(n){
-    for(int i=0; i<n; i++) set[i] = i;
-  }
-  int find(int x){
-    if (set[x] != x) set[x] = find(set[x]);
-    return set[x];
-  }
-  void union_(int x, int y){
-    set[find(y)] = find(x);
-  }
-private:
-  vector<int> set;
-};
-
 class Solution {
 public:
     vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
 
         //Search the node that has two edge point it. And the two edges.
-        vector<int> search(edges.size() + 1, -1);
-        int node, i=0;
         bool flag = false;
-        int m;
-        vector<int> edgeA, edgeB;
+        vector<int> edgeA, edgeB, parents(edges.size() + 1, 0);
 
-        for(auto edge : edges){
-            if(search[edge[1]] == -1){
-              search[edge[1]] = edge[0];
-            }else{
-                edgeA = {search[edge[1]], edge[1]};
+        for(auto & edge : edges){
+            if(parents[edge[1]] == 0){
+              parents[edge[1]] = edge[0];
+            }else{ // don't add edgB into the parent graph.
+                edgeA = {parents[edge[1]], edge[1]};
                 edgeB = edge;
-                flag = true;
-                break;
+                edge[0] = 0;
             }
-            i++; // use to recode the location of egdeB
         }
 
         // Find two edge point, Try to delete the edgeB, and then test this graph to known whether it's rooted tree.
-        if(flag){ 
-          edges.erase(edges.begin() + i); // delete edgeB from graph. 
-          if(isRootedTree(edges)) return edgeB;
-          return edgeA;
-        }else{ // To find the hoop;
-          auto hoops = get_hoops(search);
-          for(int i=0; i<edges.size(); ++i){
-            for(auto hoop : hoops){
-              if(edges[edges.size() - 1 - i][0] == hoop[0] && edges[edges.size() - 1 - i][1] == hoop[1]) return hoop;
+        for(int i=0; i<edges.size()+1; i++) parents[i] = i;
+
+        for(auto edge : edges){
+            if(edge[0] == 0) continue;
+            int r = root(parents, edge[0]);
+            if(r == edge[1]){ // make up the hoops.
+                if (!edgeB.empty()) return edgeA;
+                return edge;
             }
-          }
+            parents[edge[1]] = r;
         }
-        return {0, 0};
+        return edgeB;
     }
 
-    vector<vector<int>> get_hoops(vector<int>& parents){
-      
-      int node = parents[1];
-      vector<vector<int>> hoops;
-      // enter the hoop
-      int i=0;
-      while(i++<parents.size()){ 
-        node = parents[node];
+    int root(vector<int> parents, int k){
+      if(k!=parents[k]){
+        parents[k] = root(parents, parents[k]);
       }
-
-      // save the hoop
-      int start = node;
-      node=parents[node];
-      while (node != start){
-        hoops.push_back({parents[node], node});
-        node=parents[node];
-      }
-
-      // return the hoops
-      return hoops;
-    }
-
-    bool isRootedTree(vector<vector<int>>& edges){
-      // search root. 
-      int root_num = 0;
-      vector<bool> search(edges.size() + 1, false);
-      for(auto edge : edges){
-        search[edge[1]] = true;
-      }
-      for(auto num : search){
-        root_num += num;
-      }
-
-      return root_num == edges.size() - 1;
+      return parents[k];
     }
 };
 
@@ -127,9 +75,10 @@ int main(){
   vector<vector<int>> a = {{1, 2}, {1, 3}, {2, 3}};
   vector<vector<int>> b = {{1, 2}, {2, 3}, {3, 1}, {4, 1}};
   vector<vector<int>> c = {{1, 2}, {2, 3}, {3, 1}, {1, 4}};
+  vector<vector<int>> d = {{4, 2}, {1, 5}, {5, 2},{5, 3}, {2, 4}};
 
   Solution solu;
-  auto egde = solu.findRedundantDirectedConnection(b);
+  auto egde = solu.findRedundantDirectedConnection(d);
   cout << egde[0] << ' ' << egde[1] << endl;
   return 0;
 }
